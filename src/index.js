27cfafb0b1a2c6225926f676e1d6b2f41296927f
kik <tachanok.r@gmail.com>
2017-06-29 15:34:49 +0700

    no message

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 import './index.css';
-//
-// import {
-//     getUsers,
-//     deleteUser
-// } from './api/userApi';
-//
-// // Populate table of users via API call.
-// getUsers().then(result => {
-//     let usersBody = "";
-//
-//     result.forEach(user => {
-//         usersBody += `<tr>
-//       <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
-//       <td>${user.id}</td>
-//       <td>${user.firstName}</td>
-//       <td>${user.lastName}</td>
-//       <td>${user.email}</td>
-//       </tr>`
-//     });
-//
-//     global.document.getElementById('users').innerHTML = usersBody;
-//
+
+import {
+    getUsers
+    // ,
+    // deleteUser
+} from './api/userApi';
+
+// Populate table of users via API call.
+getUsers().then(result => {
+    let usersBody = "";
+
+    result.forEach(user => {
+        usersBody += `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+    });
+
+    global.document.getElementById('users').innerHTML = usersBody;
+
 //     const deleteLinks = global.document.getElementsByClassName('deleteUser');
 //
 //     // Must use array.from to create a real array from a DOM collection
@@ -34,10 +35,10 @@ import './index.css';
 //             row.parentNode.removeChild(row);
 //         };
 //     });
-// });
-import numeral from 'numeral';
-
-const courseValue = numeral(1000).format('$0,0.00');
-// debugger;
-/* eslint-disable no-console*/
-console.log(`I would pay ${courseValue} for this awesome course!`);
+});
+// import numeral from 'numeral';
+//
+// const courseValue = numeral(1000).format('$0,0.00');
+// // debugger;
+// /* eslint-disable no-console*/
+// console.log(`I would pay ${courseValue} for this awesome course!`);
